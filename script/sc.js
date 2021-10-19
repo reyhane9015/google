36d23edc5ebf656d1660e3b21ae5bbee7d315cb2
Reyhaneh Rafati <reyhaneh.rafati@neviseh.net>
2021-10-19 14:13:30 +0330

    google

---
+++ script/sc.js
@@ -0,0 +1,9 @@
+function openModal(e) {
+    document.querySelector('.logoBackdrop').classList.toggle('showed');
+    document.querySelector('.logoModal').classList.toggle('showed');
+}
+
+function openProfileModal(e) {
+    document.querySelector('.profileBackdrop').classList.toggle('showed');
+    document.querySelector('.profileModal').classList.toggle('showed');
+}
